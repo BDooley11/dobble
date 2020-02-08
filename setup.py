d58acc2614b5c4871d426da1067bb6de12a3b7d6
BDooley11 <55091575+BDooley11@users.noreply.github.com>
2020-02-08 18:14:15 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+requirements = [emoji,random]
+
+setup(name="dobble",
+      version="0.1",
+      description="card game dobble",
+      url="",
+      author="Barry Dooley",
+      author_email="barry.dooley@ucd.ie",
+      licence="GPL3",
+      packages=['dobble'],
+      install_requires=requirements,
+      entry_points={
+        'console_scripts':['run dobble=dobble.main:main']
+        }
+      )
